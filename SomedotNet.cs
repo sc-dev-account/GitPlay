c56789d0d5000f5927254f994de4bb0a3a742d46
SCarter <SCarter@BLACK-WIN10>
2021-03-29 20:33:02 +0100

    test3

--- SomedotNet.cs
+++ SomedotNet.cs
@@ -1,14 +1,44 @@
-usinggg System.Text
+using System.Text
 
-public class Test
+namespace Name
 {
+  
+  public class Test
+  {
     public Test()
     {
         StringBuilder s = new StringBuilder();
     }
-    public int Test { get; set; }
-    public string Name { get; set;}
+        public int Test { get; set; }
+        public string Name { get; set;}
 
-public int test2 { get; set; }  
+        public int test2 { get; set; }  
+        public int TEST { get; set; }   
+
+  
+         public int ttt { get; set; }    
+  }
+
+
+  
+    publc class Testing
+    {
+
+      public Testing(Parameters)
+      {
+        test =   4;
+      }
+      
+      int test t = 0
+    }
     
+    publc class Testing2
+    {
+
+      int test t = 0
+    }
+
+
+
 }
+
